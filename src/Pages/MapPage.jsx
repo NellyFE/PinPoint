9539b1aa3037f55b3cbafb96a999c5f96a711e89
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2025-10-18 06:58:18 +0100

    sidebar modals setup

--- src/Pages/MapPage.jsx
+++ src/Pages/MapPage.jsx
@@ -1,9 +1,48 @@
-import { Nav } from "../components/navbar"
+import { useState } from "react";
+import { Nav } from "../components/navbar";
 
 export const Mappage = () => {
-    return(
-        <section>
-            <Nav/>
-        </section>
-    )
-}
+const [destinations, setDestinations] = useState([
+
+       {
+      id: 1,
+      name: 'Tokyo, Japan',
+      lat: 35.6762,
+      lng: 139.6503,
+      notes: 'Want to experience the culture and food scene',
+      image: 'https://images.unsplash.com/photo-1540959375944-7049f642e9c1?w=300&h=300&fit=crop',
+    },
+    {
+      id: 2,
+      name: 'Barcelona, Spain',
+      lat: 41.3851,
+      lng: 2.1734,
+      notes: 'Gaudí architecture and Mediterranean beaches',
+      image: 'https://images.unsplash.com/photo-1583444694107-31cd97dfd241?w=300&h=300&fit=crop',
+    },
+])
+
+
+  return (
+    <section>
+      <Nav />
+
+      {/* main app layout container  */}
+      <div className="sidebar-layout flex h-screen bg-[#f9fafb]">
+
+        {/* left:sidebar */}
+        <div className="sidebar">
+<h2>side bar will go here</h2>
+        </div>
+
+
+        {/* right: map area */}
+        <div className="flex-1 bg-blue-300">
+            <h2>map will go here</h2>
+        </div>
+
+
+      </div>
+    </section>
+  );
+};
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,16 @@
+export default function Sidebar({destinations, setDestinations}) {
+
+    const handleAdd = () =>{
+        console.log('add button clicked')
+    }
+
+    const handleExport = () => {
+        console.log('export button clicked')
+    }
+
+    return(
+        <div className="sidebar">
+
+        </div>
+    )
+}
