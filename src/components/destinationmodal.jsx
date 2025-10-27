0d280602a2802956e7e5bfae3f19798324700465
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2025-10-27 17:50:17 +0100

    handleChange

--- src/components/destinationmodal.jsx
+++ src/components/destinationmodal.jsx
@@ -1,5 +1,24 @@
-export const DestinationModal = () => {
+import { useState } from "react"
+
+export const DestinationModal = ({onClose,onSave}) => {
+
+
+const [formData,setFormData] = useState({
+    name:"",
+    lat:"",
+    lng:"",
+    image:"",
+    notes:""
+})
+
+const handleChange = () => {
+    const {name,value} = e.target;
+    setFormData({...formData,[name]:value})
+}
+
     return(
-        <div></div>
+        <div>
+
+        </div>
     )
 }
