33d050b475bc90600f0d4aaf96d2684964f0d720
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2025-10-30 10:51:19 +0100

    destinationa modal

--- src/components/destinationmodal.jsx
+++ src/components/destinationmodal.jsx
@@ -52,7 +52,7 @@ onSave(newDestination)
                 </div>
 
                 {/* Modal Body */}
-                <div className="mb-6 p-4">
+                <div className="mb-4 p-4">
                     <label className="block text-sm text-[#374151] font-semibold">
                         Destination Name *
                     </label>
@@ -63,15 +63,15 @@ onSave(newDestination)
                     placeholder="e.g Tokyo, Japan"
                     value={formData.name}
                     onClick={handleChange}
-                    className="w-full border border-[#d1d5db] p-2 rounded-lg box-border"
+                    className="w-full border border-[#d1d5db] outline-[#e5f5e0] p-2 rounded-lg box-border"
                     />
                 </div>
 
                 {/* Coordinates */}
-                <div className="grid grid-cols-[1fr 1fr] gap-2 mb-4">
+                <div className="flex flex-col lg:flex-row gap-2 mb-4 px-4">
                     <div>
                     <label className="block text-sm text-[#374151] font-semibold">
-                        Coordinates
+                        Latitude *
                     </label>
 
                     <input 
@@ -81,7 +81,7 @@ onSave(newDestination)
                     placeholder="35.6762" 
                     value={formData.lat}
                     onClick={handleChange}
-                    className="w-full border border-[#d1d5db] p-2 rounded-lg box-border"
+                    className="w-full border border-[#d1d5db] outline-[#e5f5e0] p-2 rounded-lg box-border"
 />
                 </div>
          
@@ -98,9 +98,63 @@ onSave(newDestination)
                 placeholder="139.6503"
                 value={formData.lng}
                 onChange={handleChange}
-                className="w-full border border-[#d1d5db] p-2 rounded-lg box-border"
+                className="w-full border border-[#d1d5db] outline-[#e5f5e0] p-2 rounded-lg box-border"
                 />
             </div>
+
+               </div>
+
+               {/* Notes */}
+               <div className="mb-4 p-4">
+                <label className="block text-sm text-[#374151] font-semibold">
+                    Notes *
+                </label>
+
+                <textarea 
+                name="notes" 
+                placeholder="share your travel goals..."
+                id=""
+                value={formData.value}
+                onChange={handleChange}
+                className="w-full h-[96px] border border-[#d1d5db] p-2 outline-[#e5f5e0] rounded-md text-sm resize-none box-border">
+
+                </textarea>
+
+               </div>
+
+               {/* Image Url */}
+               <div className="mb-4 px-4">
+                <label>
+                    Image URL (Optional)
+                </label>
+                
+                <input 
+                type="text"
+                name="image"
+                placeholder="paste image url here"
+                value={formData.image}
+                onChange={handleChange} 
+                className="w-full border border-[#d1d5db] outline-[#e5f5e0] p-2 rounded-lg box-border"
+                />
+                
+               </div>
+
+               {/* Tips */}
+               <div className="mb-4 px-4">
+               <div className="p-2 bg-[#eff6ff] border border-[#bfdbfe] rounded-xl text-sm text-[#1e40af]">
+                <p className="font-semibold mb-1">💡 Need coordinates?</p>
+                <p>Search your destination on Google Maps, click on it, and the coordinates appear in the URL!</p>
+               </div>
+               </div>
+
+               {/* Modal Footer */}
+               <div className="modal-footer">
+                <button onClick={onClose}
+                className="flex flex-1 bg-[#e5e7eb] text-[#1f2937] text-center items-center justify-center p-2 rounded-lg font-semibold border border-transparent cursor-pointer hover:border-red-400">
+                    Cancel
+                    </button>
+                <button onClick={handleSave}
+                className="flex flex-1 bg-[#41ab5d] text-center items-center justify-center text-white p-2 rounded-lg font-semibold border-none cursor-pointer">Add</button>
 
                </div>
 
